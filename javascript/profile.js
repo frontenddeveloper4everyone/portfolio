e70970c58071a1a059fe3a3bdeadca8636113d28
Bekalu Eshete <bekelueshete@gmail.com>
2024-04-09 13:55:13 +0300

    my profile

--- javascript/profile.js
+++ javascript/profile.js
@@ -25,3 +25,43 @@ btns.forEach((btn, index) => {
         this.classList.add("active");
     });
 });
+
+const next = document.querySelector(".next");
+const back = document.querySelector(".back");
+const carousel   = document.querySelector(".carousel-cards");
+let  angle=0;
+next.addEventListener("click",()=>{
+    angle -=45;
+    carousel.style.transform=`translateZ(-25rem)
+    rotateY(${angle}deg)`;
+})
+back.addEventListener("click",()=>{
+    angle +=45;
+    carousel.style.transform=`translateZ(-25rem)
+    rotateY(${angle}deg)`;
+})
+//    mobile 
+ const btnNav= document.querySelector(".btn-mobile-nav");
+ const header= document.querySelector(".header");
+ btnNav.addEventListener("click",function(){
+    header.classList.toggle("nav-open")
+ });
+//  creating stiky navigation
+const stickHero=document.querySelector(".section-hero")
+ const obs= new IntersectionObserver(function (entries){
+    const ent=entries[0];
+    console.log(ent);
+    if(ent.isIntersecting===false){
+        document.querySelector(".header").classList.add("sticky")
+    }
+    if(ent.isIntersecting===true){
+        document.querySelector(".header").classList.remove("sticky")
+    }
+
+ }, 
+ { 
+    roo:null,
+    threshold:0, 
+    rootMargin: "-80px",
+ });
+ obs.observe(stickHero);
